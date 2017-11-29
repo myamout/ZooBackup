cfaca77afea1807b4ff4d1b965e9a4c3d8383966
Michael Green <me@michaelgreen.net>
2017-11-28 16:30:58 -0800

    Startup scripts, seeding scripts, elastic config, more automation thats cross platform

--- start_servers.sh
+++ start_servers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-elasticsearch &
-mongod &
+mkdir -p ./data
+# Starting elastic search with this project's spec
+elasticsearch -Dconfig=`pwd`"/elasticsearch" &
+# Data dir in the current dir rather than in the root dir
+mongod --dbpath="./data" &
