983c52f111870a3f9134b706b7e1e51f88f6ac5a
Michael Green <me@michaelgreen.net>
2017-11-29 00:57:22 -0800

    logo update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export const Header = () => (
     <header>
 
       <nav className="navbar navbar-expand-lg navbar-inverse">
-        <img src="../media/zoo.jpeg" className="navLogoSize" alt=""></img>
+        <img src="../media/more_consistent_logo.png" className="navLogoSize" alt=""></img>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
